20618d80708957e39a4488e5a33a61b8c4da8722
USER <USER@L04.r1804.tciii.org.tw>
2021-04-28 11:58:15 +0800

    04281158

--- MyJavaEE/src/main/java/tw/brad/javaee/Brad18.java
+++ MyJavaEE/src/main/java/tw/brad/javaee/Brad18.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -11,6 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import tw.brad.utils.BCrypt;
 
 /**
  * Servlet implementation class Brad18
@@ -28,7 +31,7 @@ public class Brad18 extends HttpServlet {
 			prop.put("user", "root");
 			prop.put("password", "root");
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/", prop);
+					"jdbc:mysql://localhost:3306/iii", prop);
     		System.out.println("Connect OK");
 
     	}catch (Exception e) {
@@ -39,22 +42,39 @@ public class Brad18 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		try {
-			int rows = add2MySQL();
+			if (!isAccountDup("amy")) {
+				int rows = add2MySQL("amy","123456","Amy");
+				System.out.println(rows);
+			}else {
+				System.out.println("account dup.");
+			}
 		}catch(Exception e) {
 			System.out.println(e);
 		}
 	}
 	
-	private int add2MySQL() throws Exception {
+	private boolean isAccountDup(String account) throws Exception {
+		String sql = "SELECT count(*) as count FROM user WHERE account = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, account);
+		ResultSet rs = pstmt.executeQuery();
+		rs.next();
+		int count = rs.getInt("count");
+		
+		return count > 0;
+	}
+	
+	private int add2MySQL(String account, String passwd, String realname) 
+			throws Exception {
 		if (conn == null && conn.isClosed()) {
 			throw new Exception();
 		}
 		
 		String sql = "INSERT INTO user (account,passwd,realname) VALUES (?,?,?)";
 		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, ?);
-		pstmt.setString(2, ?);
-		pstmt.setString(3, ?);
+		pstmt.setString(1, account);
+		pstmt.setString(2, BCrypt.hashpw(passwd, BCrypt.gensalt()));
+		pstmt.setString(3, realname);
 		int count = pstmt.executeUpdate();
 		return count;
 	}
