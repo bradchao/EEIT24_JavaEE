7f3fa3055344672566fa7c8dd24cc9c8c0fe8b77
USER <USER@L04.r1804.tciii.org.tw>
2021-04-29 12:09:11 +0800

    04291209

---
+++ MyJavaEE/src/main/java/tw/brad/utils/Member.java
@@ -0,0 +1,40 @@
+package tw.brad.utils;
+
+public class Member {
+	private String account, realname;
+	private int age;
+	
+	public Member() {
+		this("","",18);
+	}
+	
+	public Member(String account, String realname, int age) {
+		this.account = account;
+		this.realname = realname;
+		this.age = age;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void addAge() {
+		this.age++;
+	}
+	
+}
