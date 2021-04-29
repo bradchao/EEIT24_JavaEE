1f8606f9531c6ab705f450697e4a379f0c566379
USER <USER@L04.r1804.tciii.org.tw>
2021-04-29 15:58:07 +0800

    04291558

--- MyJavaEE/src/main/java/tw/brad/utils/BradAPIs.java
+++ MyJavaEE/src/main/java/tw/brad/utils/BradAPIs.java
@@ -8,4 +8,20 @@ public class BradAPIs {
 	public static String sayYa(String name) {
 		return "Hello, " + name;
 	}
+	public static String calc(String x, String y, String op) {
+		String result = "";
+		try {
+			int intX = Integer.parseInt(x);
+			int intY = Integer.parseInt(y);
+			switch(op) {
+				case "1": result = (intX + intY) + ""; break;
+				case "2": result = (intX - intY) + ""; break;
+				case "3": result = (intX * intY) + ""; break;
+				case "4": result = (intX / intY) + ""; break;
+			}
+		}catch(Exception e) {
+		}
+		
+		return result;
+	}
 }
